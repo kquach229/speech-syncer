e76c3d9941149d414672f3a1588fceb6a646c4aa
kquach229 <kquach229>
2024-09-19 22:03:46 -0400

    save

--- app/hooks/useTranslate.tsx
+++ app/hooks/useTranslate.tsx
@@ -1,26 +1,24 @@
-import { useEffect, useState, useCallback } from "react";
-import OpenAI from "openai";
+import { useEffect, useState, useCallback } from 'react';
+import OpenAI from 'openai';
 
 // Ensure that the OpenAI API key is set
 const openai = new OpenAI({
-  apiKey:
-    "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" ||
-    "", // Use environment variable
+  apiKey: process.env.OPENAI_API_KEY || '', // Use environment variable
   dangerouslyAllowBrowser: true, // Understand the implications of using this option
 });
 
 const useTranslate = (sourceText: string, selectedLanguage: string): string => {
-  const [targetText, setTargetText] = useState("");
+  const [targetText, setTargetText] = useState('');
 
   // Function to handle translation
   const handleTranslate = useCallback(
     async (text: string) => {
       try {
         const response = await openai.chat.completions.create({
-          model: "gpt-3.5-turbo",
+          model: 'gpt-3.5-turbo',
           messages: [
             {
-              role: "user",
+              role: 'user',
               content: `Translate this to ${selectedLanguage}: ${text}`,
             },
           ],
@@ -29,17 +27,17 @@ const useTranslate = (sourceText: string, selectedLanguage: string): string => {
 
         const data =
           response.choices[0].message.content ||
-          "Api error: you do not have access to the chat-gpt4 plan or your credits have run out. Please upgrade your billing plan to continue using";
+          'Api error: you do not have access to the chat-gpt4 plan or your credits have run out. Please upgrade your billing plan to continue using';
 
         setTargetText(data);
       } catch (error) {
         // Use 'unknown' for error to handle dynamic error types
         if (error instanceof Error) {
-          console.error("Error translating text:", error.message);
-          setTargetText("Translation error: " + error.message); // Provide user-friendly error message
+          console.error('Error translating text:', error.message);
+          setTargetText('Translation error: ' + error.message); // Provide user-friendly error message
         } else {
-          console.error("Unknown error translating text");
-          setTargetText("Unknown error occurred during translation.");
+          console.error('Unknown error translating text');
+          setTargetText('Unknown error occurred during translation.');
         }
       }
     },
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,34 @@
-"use client";
-import "regenerator-runtime/runtime";
-import TextArea from "./components/inputs/TextArea";
-import { ChangeEvent, useState } from "react";
+'use client';
+import 'regenerator-runtime/runtime';
+import TextArea from './components/inputs/TextArea';
+import { ChangeEvent, useState } from 'react';
 import {
   FaVolumeUp,
   FaCopy,
   FaThumbsUp,
   FaThumbsDown,
   FaStar,
-} from "react-icons/fa";
-import SpeechRecognitionComponent from "./components/speechRecognition/SpeechRecognitionComponent";
-import FileUpload from "./components/inputs/FileUpload";
-import { rtfToText } from "./utils/rtfToText";
-import LinkPaste from "./components/inputs/LinkPaste";
-import useTranslate from "./hooks/useTranslate";
-import LanguageSelector from "./components/inputs/LanguageSelector";
-import CategoryLinks from "./components/CategoryLinks";
+} from 'react-icons/fa';
+import SpeechRecognitionComponent from './components/speechRecognition/SpeechRecognitionComponent';
+import FileUpload from './components/inputs/FileUpload';
+import { rtfToText } from './utils/rtfToText';
+import LinkPaste from './components/inputs/LinkPaste';
+import useTranslate from './hooks/useTranslate';
+import LanguageSelector from './components/inputs/LanguageSelector';
+import CategoryLinks from './components/CategoryLinks';
 
 export default function Home() {
-  const [sourceText, setSourceText] = useState<string>("");
+  const [sourceText, setSourceText] = useState<string>('');
   const [copied, setCopied] = useState<boolean>(false);
   const [favorite, setFavorite] = useState<boolean>(false);
   const [languages] = useState<string[]>([
-    "English",
-    "Spanish",
-    "French",
-    "German",
-    "Chinese",
+    'English',
+    'Spanish',
+    'French',
+    'German',
+    'Chinese',
   ]);
-  const [selectedLanguage, setSelectedLanguage] = useState<string>("Spanish");
+  const [selectedLanguage, setSelectedLanguage] = useState<string>('Spanish');
 
   const targetText = useTranslate(sourceText, selectedLanguage);
 
@@ -54,9 +54,9 @@ export default function Home() {
   const handleFavorite = () => {
     setFavorite(!favorite);
     if (!favorite) {
-      localStorage.setItem("favoriteTranslation", targetText);
+      localStorage.setItem('favoriteTranslation', targetText);
     } else {
-      localStorage.removeItem("favoriteTranslation");
+      localStorage.removeItem('favoriteTranslation');
     }
   };
 
@@ -66,54 +66,54 @@ export default function Home() {
   };
 
   return (
-    <div className="h-screen w-full dark:bg-darkBlue bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
-      <div className="absolute inset-0 flex items-center justify-center pointer-events-none dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <div className="relative overflow-hidden">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 py-10 sm:py-24">
-          <div className="text-center">
-            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold text-neutral-200">
-              Speech<span className="text-lightBlue">Syncer</span>
+    <div className='h-screen w-full dark:bg-darkBlue bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center'>
+      <div className='absolute inset-0 flex items-center justify-center pointer-events-none dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
+      <div className='relative overflow-hidden'>
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 py-10 sm:py-24'>
+          <div className='text-center'>
+            <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold text-neutral-200'>
+              Speech<span className='text-lightBlue'>Syncer</span>
             </h1>
-            <p className="mt-3 text-neutral-400 text-lg">
+            <p className='mt-3 text-neutral-400 text-lg'>
               SpeechSyncer: Bridging the gap between languages
             </p>
-            <div className="mt-7 sm:mt-12 mx-auto max-w-4xl">
-              <div className="grid gap-4 md:grid-cols-2 grid-cols-1">
-                <div className="relative flex flex-col border rounded-lg shadow-lg bg-neutral-900 border-neutral-700 shadow-gray-900/20">
+            <div className='mt-7 sm:mt-12 mx-auto max-w-4xl'>
+              <div className='grid gap-4 md:grid-cols-2 grid-cols-1'>
+                <div className='relative flex flex-col border rounded-lg shadow-lg bg-neutral-900 border-neutral-700 shadow-gray-900/20'>
                   <TextArea
-                    id={"source-language"}
-                    placeholder="Source Language"
+                    id={'source-language'}
+                    placeholder='Source Language'
                     value={sourceText}
                     onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
                       setSourceText(e.target.value);
                     }}
                   />
-                  <div className="flex flex-col md:flex-row justify-between p-4">
-                    <span className="flex space-x-2 items-center">
+                  <div className='flex flex-col md:flex-row justify-between p-4'>
+                    <span className='flex space-x-2 items-center'>
                       <SpeechRecognitionComponent
                         setSourceText={setSourceText}
                       />
                       <FaVolumeUp
                         size={22}
-                        className="text-gray-400 cursor-pointer"
+                        className='text-gray-400 cursor-pointer'
                         onClick={() => handleAudioPlayback(sourceText)}
                       />
                       <FileUpload handleFileUpload={handleFileUpload} />
                     </span>
-                    <span className="text-sm text-gray-400">
+                    <span className='text-sm text-gray-400'>
                       {sourceText.length}/2000
                     </span>
                   </div>
                 </div>
-                <div className="relative flex flex-col border rounded-lg shadow-lg bg-neutral-900 border-neutral-700 shadow-gray-900/20">
+                <div className='relative flex flex-col border rounded-lg shadow-lg bg-neutral-900 border-neutral-700 shadow-gray-900/20'>
                   <TextArea
-                    id={"target-language"}
+                    id={'target-language'}
                     value={targetText}
-                    placeholder="Target Language"
+                    placeholder='Target Language'
                     onChange={() => {}}
                   />
-                  <div className="flex flex-col md:flex-row justify-between p-4">
-                    <span className="flex space-x-2 items-center">
+                  <div className='flex flex-col md:flex-row justify-between p-4'>
+                    <span className='flex space-x-2 items-center'>
                       <LanguageSelector
                         languages={languages}
                         selectedLanguage={selectedLanguage}
@@ -121,32 +121,32 @@ export default function Home() {
                       />
                       <FaVolumeUp
                         size={22}
-                        className="text-gray-400 cursor-pointer"
+                        className='text-gray-400 cursor-pointer'
                         onClick={() => handleAudioPlayback(targetText)}
                       />
-                      <div className="flex space-x-2 items-center">
+                      <div className='flex space-x-2 items-center'>
                         <FaCopy
                           size={22}
-                          className="text-gray-400 cursor-pointer"
+                          className='text-gray-400 cursor-pointer'
                           onClick={handleCopyToClipboard}
                         />
                         {copied && (
-                          <span className="text-xs text-green-500 ml-2">
+                          <span className='text-xs text-green-500 ml-2'>
                             Copied!
                           </span>
                         )}
-                        <FaThumbsUp size={22} className="cursor-pointer" />
-                        <FaThumbsDown size={22} className="cursor-pointer" />
+                        <FaThumbsUp size={22} className='cursor-pointer' />
+                        <FaThumbsDown size={22} className='cursor-pointer' />
                         <FaStar
                           className={`cursor-pointer ${
-                            favorite ? "text-yellow-500" : "text-gray-400"
+                            favorite ? 'text-yellow-500' : 'text-gray-400'
                           }`}
                           onClick={handleFavorite}
                           size={22}
                         />
                       </div>
                     </span>
-                    <span className="text-sm text-gray-400">
+                    <span className='text-sm text-gray-400'>
                       {targetText.length}/2000
                     </span>
                   </div>
